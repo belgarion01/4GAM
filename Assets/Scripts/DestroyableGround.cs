5a906a455fa7604715848066f8ecd10791e14d4f
belgarion01 <hugo.raguin@gmail.com>
2019-09-15 12:49:56 +0200

    no message

--- Assets/Scripts/DestroyableGround.cs
+++ Assets/Scripts/DestroyableGround.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Experimental.VFX;
 
 public class DestroyableGround : MonoBehaviour
 {
-    public UnityEvent OnFalling;
+    public UnityEvent OnHit;
+    public VisualEffect smokeVFX;
+    public int smokePerHit;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,7 +16,18 @@ public class DestroyableGround : MonoBehaviour
             GetComponent<Rigidbody>().isKinematic = false;
             GetComponent<Rigidbody>().useGravity = true;
             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-            OnFalling.Invoke();
+            for (int i = 0; i < smokePerHit; i++)
+            {
+                VisualEffect vfx = Instantiate(smokeVFX, GetRandomPointSphere(collision.contacts[0].point, 1), Quaternion.identity);
+                Destroy(vfx, 10f);
+            }         
+            //vfx.SendEvent("OnPlay");           
+            OnHit.Invoke();
         }
+    }
+
+    Vector3 GetRandomPointSphere(Vector3 center, float radius) {
+        Vector3 point = center + Random.insideUnitSphere * radius;
+        return point;
     }
 }
