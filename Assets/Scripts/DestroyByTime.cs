4ae653b875b2b665c2375ad79c2238bd65e1fc4b
belgarion01 <hugo.raguin@gmail.com>
2019-09-15 10:55:11 +0200

    no message

--- Assets/Scripts/DestroyByTime.cs
+++ Assets/Scripts/DestroyByTime.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using UnityEngine.Events;
 
 public class DestroyByTime : MonoBehaviour
 {
--- Assets/Scripts/PrefabLauncher.cs
+++ Assets/Scripts/PrefabLauncher.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PrefabLauncher : MonoBehaviour
 {
@@ -10,29 +12,53 @@ public class PrefabLauncher : MonoBehaviour
     public float timeBeforeLaunch = 1f;
     public float launchForce = 20f;
 
-    public LayerMask groundMask;
+    public float lauchCooldown;
+    private float currentLaunchCooldown;
+    public Slider cooldownSlider;
+    bool launchReady = true;
 
-    
+    public LayerMask clickableMask;
 
-    void Start()
+    public UnityEvent OnClickSucces;
+    public UnityEvent OnClickFail;
+    public UnityEvent OnLaunching;
+    public UnityEvent OnLaunchReady;
+
+    private void Awake()
     {
-        
+        cooldownSlider.maxValue = lauchCooldown;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        if (currentLaunchCooldown < lauchCooldown) currentLaunchCooldown += Time.deltaTime;
+        launchReady = currentLaunchCooldown < lauchCooldown ? false : true;
         Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(mouseRay, out hit, Mathf.Infinity, groundMask) && Input.GetMouseButtonDown(0)) {
-            Instantiate(targetPrefab, hit.point, Quaternion.Euler(-90f, 0f, 0f));
-            StartCoroutine(LaunchPrefab(mouseRay.direction));
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(mouseRay, out hit, Mathf.Infinity, clickableMask)&&launchReady)
+            {
+                Instantiate(targetPrefab, hit.point, Quaternion.Euler(-90f, 0f, 0f));
+                StartCoroutine(LaunchPrefab(mouseRay.direction));
+                currentLaunchCooldown = 0f;
+                OnClickSucces.Invoke();
+            }
+            else {
+                OnClickFail.Invoke();
+            }
         }
+        UpdateUI();
     }
 
     IEnumerator LaunchPrefab(Vector3 direction) {
         yield return new WaitForSeconds(timeBeforeLaunch);
         GameObject bol = Instantiate(instantiatedPrefab, transform.position, Quaternion.identity);
         bol.GetComponent<Rigidbody>().AddForce(direction * launchForce, ForceMode.VelocityChange);
+        OnLaunching.Invoke();
+    }
+
+    void UpdateUI() {
+        cooldownSlider.value = currentLaunchCooldown;
     }
 }
